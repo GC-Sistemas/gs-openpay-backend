563365250d316c4b85c87c774f5906df3a69e5bc
Omar Gonzalez <omargonzalez9911@yahoo.com>
2020-02-20 10:33:56 -0500

    Fix #1 - Se diversifica el flujo de trabajo del procesamiento de cargos

--- app/Transaction.php
+++ app/Transaction.php
@@ -9,4 +9,8 @@ class Transaction extends Model
     public function user(){
         return $this->belongsTo('App\User', 'user_id');
     }
+
+    public function client(){
+        return $this->belongsTo('App\Client', 'client_id');
+    }
 }
